d4bb605a705c9dd0ff91727eec4ee6a9db6fb8d4
jitulal89 <jitulalwani89@gmail.com>
2026-08-26 00:57:53 +0530

    Update Supabase URL and anon key in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'https://YOUR-PROJECT.supabase.co';
-export const SUPABASE_ANON_KEY = 'YOUR-ANON-KEY';
+export const SUPABASE_URL = 'https://bfalondvpfijthberomf.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmYWxvbmR2cGZpanRoYmVyb21mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2ODAzNDgsImV4cCI6MjEwMzI1NjM0OH0.MZCmFjZXD5bM_JcaV5jbduBZV8f9hemfUs3jh2x1xBk';
